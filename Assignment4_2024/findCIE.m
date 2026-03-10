f69832521bd350846b313e68a462395da9b8184a
Ariel Mobius <72271825+akmobius@users.noreply.github.com>
2026-03-10 11:22:10 -0400

    adding pset4

---
+++ Assignment4_2024/findCIE.m
@@ -0,0 +1,22 @@
+% Function to calculate the CIE x,y chromaticity coordinates from a reflectance
+% spectrum
+% 
+% ------------------
+% >>>>> INPUTS <<<<<
+% ------------------
+% omega = angular frequency [rad/s].
+% R = reflectance [1].
+% 
+% ------------------
+% <<<<< OUTPUT >>>>>
+% ------------------
+% x,y = CIE chromaticity coordinates [1].
+% 
+function [x,y] = findCIE(omega, R)
+    % For the CIE x, y, and z color matching curves, use the CIE 1931 
+    % standard observer equations from the provided PDF "Simple Analytic 
+    % Approximations to the CIE XYZ Color Matching Functions".
+
+    % === TYPE YOUR CODE BELOW === %
+
+end
---
+++ Assignment4_2024/maxwellGarnett.m
@@ -0,0 +1,18 @@
+% Function to calculate the effective index of a porous material
+% 
+% ------------------
+% >>>>> INPUTS <<<<<
+% ------------------
+% eps_m = metal permittivity [1] (vector or double)
+% eps_a = ambient permittivity in pores [1] (vector or double)
+% p = porosity, percent reported in decimal [1] (vector or double)
+% ------------------
+% <<<<< OUTPUT >>>>>
+% ------------------
+% eps_ff = effectric permittivity of the medium [1] (vector)
+% 
+
+function [eps_eff] = maxwellGarnett(eps_m,eps_a,p)
+        % === TYPE YOUR CODE BELOW === %
+        
+end
