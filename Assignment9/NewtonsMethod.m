b3aaf9cb65047a0789dc4babc5d714a02bd3fa25
Ariel Mobius <72271825+akmobius@users.noreply.github.com>
2026-05-04 20:00:50 -0400

    pset9 (and uploaded pset6)

--- Assignment9/NewtonsMethod.m
+++ Assignment9/NewtonsMethod.m
@@ -18,15 +18,49 @@
 function [solution, error] = NewtonsMethod(g, x_0, tolerance, n)
     % === TYPE YOUR CODE BELOW === %
     
+    % Initialize variables
+    current_x = x_0;
+    error = zeros(n, 1);
+    solution = x_0;
     
+    % Calculate the derivative of g symbolically
+    dg = diff(g);
     
+    for i = 1:n
+        % Evaluate g(x) and g'(x) at the current x
+        g_val = double(subs(g, current_x));
+        dg_val = double(subs(dg, current_x));
+        
+        % Avoid division by zero if the derivative is too small
+        if abs(dg_val) < 1e-12
+            fprintf('Warning: Derivative is near zero. Newton''s method may fail.\n');
+            error = error(1:i-1);
+            return;
+        end
+        
+        % Newton-Raphson update formula
+        next_x = current_x - (g_val / dg_val);
+        
+        % Calculate percent relative error
+        if next_x ~= 0
+            current_error = abs((next_x - current_x) / next_x) * 100;
+        else
+            current_error = abs(next_x - current_x) * 100;
+        end
+        
+        % Store error and update values
+        error(i) = current_error;
+        solution = next_x;
+        current_x = next_x;
+        
+        % Check for convergence
+        if current_error < tolerance
+            error = error(1:i);
+            return;
+        end
+    end
     
-    
-    
-    
-    
-    
-    
-    
-    
+    if i == n
+        fprintf('Warning: Maximum iterations reached without meeting tolerance.\n');
+    end
 end
--- Assignment9/fixedPointIteration.m
+++ Assignment9/fixedPointIteration.m
@@ -1,31 +1,40 @@
-% Function to solve an equation using fixed point iteration. In order to
-% use this function, you must rewrite the equation in the form f(x) = x.
-% 
-% [[[[[[[[[]]]]]]]]]
-% ]]]]] INPUTS [[[[[
-% [[[[[[[[[]]]]]]]]]
-% f = left-hand side of the equation f(x) = x, passed as a symbolic object.
-% x_0 = initial guess.
-% tolerance = percent error at which the equation is considered solved.
-% n = maximum number of iterations.
-% 
-% ]]]]]]]]] [[[[[[[[[
-% [[[[[ OUTPUTS ]]]]]
-% ]]]]]]]]] [[[[[[[[[
-% solution = the solution of the equation.
-% error = percent error as a function of iteration number.
-% 
 function [solution, error] = fixedPointIteration(f, x_0, tolerance, n)
     % === TYPE YOUR CODE BELOW === %
     
+    % Initialize variables
+    current_x = x_0;
+    error = zeros(n, 1); % Preallocate error array for efficiency
+    solution = x_0;      % Default solution to initial guess
     
+    for i = 1:n
+        % Evaluate the function at the current x
+        % f is a symbolic object, so we substitute current_x into it
+        next_x = double(subs(f, current_x));
+        
+        % Calculate percent relative error
+        if next_x ~= 0
+            current_error = abs((next_x - current_x) / next_x) * 100;
+        else
+            current_error = abs(next_x - current_x) * 100;
+        end
+        
+        % Store error
+        error(i) = current_error;
+        
+        % Update solution and current x
+        solution = next_x;
+        current_x = next_x;
+        
+        % Check for convergence
+        if current_error < tolerance
+            % Trim the error array to the number of iterations actually performed
+            error = error(1:i);
+            return;
+        end
+    end
     
-    
-    
-    
-    
-    
-    
-    
-    
+    % Warning if the maximum number of iterations is reached without convergence
+    if i == n
+        fprintf('Warning: Maximum iterations reached without meeting tolerance.\n');
+    end
 end
